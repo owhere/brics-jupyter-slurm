13fc293ffc9e2bc6c99d53147293e90e8201e318
James C. Womack <J.C.Womack@bristol.ac.uk>
2024-10-07 12:51:31 +0100

    Allow alternative container engine command in run scripts
    Also update image tag and container name to be consistent with repo name

--- run.sh
+++ run.sh
@@ -1,7 +1,22 @@
 #! /bin/bash
+set -euo pipefail
 
+# To use an alternative container engine command, specify as first argument, e.g.
+#   run.sh podman
+# Default with no argument is to use docker
+if (( $# == 0 )); then
+  ENGINE_CMD="docker"
+elif (( $# == 1 )); then
+  ENGINE_CMD="$1"
+else
+  echo "Error: Incorrect number of arguments"
+  echo "Usage: $0 [container engine command]"
+  exit 1
+fi
 
-docker run --name slurm-jupyter \
+set -o xtrace
+
+${ENGINE_CMD} run --name jupyter-slurm \
     --privileged \
     -p 6817:6817 -p 6818:6818 \
     -p 38024:38024   \
@@ -14,7 +29,9 @@ docker run --name slurm-jupyter \
     --mount type=bind,source=$PWD/jupyter_config,target=/srv/jupyterhub \
     --mount type=bind,source=$PWD/jupyter_notebooks,target=/tmp/admin/notebooks \
     --mount type=bind,source=$PWD/jupyter_bin/brics_slurm_spawner.py,target=/usr/local/bin/brics_slurm_spawner.py \
-    brics_slurm_jupyter:latest \
+    brics-jupyter-slurm:latest \
     jupyterhub -f /srv/jupyterhub/jupyterhub_config.py --debug
 
 #   sleep infinity (for debugging)
+
+set +o xtrace
--- run_slurm.sh
+++ run_slurm.sh
@@ -1,7 +1,22 @@
 #! /bin/bash
+set -euo pipefail
 
+# To use an alternative container engine command, specify as first argument, e.g.
+#   run.sh podman
+# Default with no argument is to use docker
+if (( $# == 0 )); then
+  ENGINE_CMD="docker"
+elif (( $# == 1 )); then
+  ENGINE_CMD="$1"
+else
+  echo "Error: Incorrect number of arguments"
+  echo "Usage: $0 [container engine command]"
+  exit 1
+fi
 
-docker run --name slurm-jupyter \
+set -o xtrace
+
+${ENGINE_CMD} run --name jupyter-slurm \
     --privileged \
     -p 6817:6817 -p 6818:6818 \
     -p 38024:38024   \
@@ -15,7 +30,9 @@ docker run --name slurm-jupyter \
     --mount type=bind,source=$PWD/jupyter_notebooks,target=/tmp/admin/notebooks \
     --mount type=bind,source=$PWD/jupyter_bin/brics_slurm_spawner.py,target=/usr/local/bin/brics_slurm_spawner.py \
     --mount type=bind,source=$PWD/jupyter_bin/brics_token_authenticator.py,target=/usr/local/bin/brics_token_authenticator.py \
-    brics_slurm_jupyter:latest \
+    brics-jupyter-slurm:latest \
     jupyterhub -f /srv/jupyterhub/jupyterhub_config_slurm.py 
-    
+
 #   sleep infinity (for debugging)
+
+set +o xtrace
