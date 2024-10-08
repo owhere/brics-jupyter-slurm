1e1455f4ca7b345b7e3d5af277952d15f1748a10
Fan Yang <fan.yang.oxford@gmail.com>
2024-10-08 19:10:06 +0100

    fix slurm job status unrecognisable

--- jupyter_config/jupyterhub_config_slurm.py
+++ jupyter_config/jupyterhub_config_slurm.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import sys
+import batchspawner
 
 sys.path.append('/usr/local/bin/') 
 from brics_slurm_spawner import BricsSlurmSpawner
@@ -24,7 +25,7 @@ spawner.user = type('User', (object,), {'name': 'admin'})()
 c.JupyterHub.spawner_class = BricsSlurmSpawner
 
 # Configure the spawner's environment and notebook settings
-c.BricsSlurmSpawner.cmd = ['jupyter-lab']
+#c.BricsSlurmSpawner.cmd = ['jupyter-lab']
 c.BricsSlurmSpawner.args = ['--notebook-dir=/tmp/admin/notebooks', '--ip=0.0.0.0', '--allow-root']
 c.BricsSlurmSpawner.debug = True
 c.BricsSlurmSpawner.default_url = '/lab'
@@ -32,9 +33,11 @@ c.BricsSlurmSpawner.ip = '0.0.0.0'
 c.BricsSlurmSpawner.notebook_dir = '/tmp/admin/notebooks'
 c.BricsSlurmSpawner.start_timeout = 300  
 c.BricsSlurmSpawner.http_timeout = 300  
-c.BricsSlurmSpawner.job_status = ['R']   # Specify the status that indicates running ('R' for Slurm)
+c.BricsSlurmSpawner.poll_interval = 10  # Set polling interval to 10 seconds
+
 
 # Slurm settings
 c.BricsSlurmSpawner.batch_query_cmd = 'squeue -j {job_id}'  # Query job status
 c.BricsSlurmSpawner.batch_cancel_cmd = 'scancel {job_id}'   # Cancel job
 
+c.JupyterHub.log_level = logging.DEBUG
